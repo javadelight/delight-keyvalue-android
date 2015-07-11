e74b71f63b7ebbefc80fe9abb8330343bba61ae0
mxro <mxro@nowhere.com>
2015-07-12 09:22:17 +1000

    Reworking generic operations on stores Autocommit Sun Jul 12 09:22:17 EST 2015

--- src/main/java/de/mxro/async/map/android/tests/AndroidStorageTests.java
+++ src/main/java/de/mxro/async/map/android/tests/AndroidStorageTests.java
@@ -1,5 +1,58 @@
 package de.mxro.async.map.android.tests;
 
+import android.database.sqlite.SQLiteDatabase;
+import de.mxro.async.map.android.AsyncMapAndorid;
+import de.mxro.async.map.android.SQLiteConfiguration;
+import de.mxro.serialization.Serializer;
+import de.mxro.serialization.jre.SerializationJre;
+import de.mxro.serialization.jre.StreamDestination;
+import de.mxro.serialization.jre.StreamSource;
+import delight.async.AsyncCommon;
+import delight.async.Operation;
+import delight.async.callbacks.SimpleCallback;
+import delight.async.callbacks.ValueCallback;
+import delight.async.jre.Async;
+import delight.functional.Success;
+import delight.keyvalue.Store;
+import delight.keyvalue.tests.StoreTest;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
+import org.robolectric.shadows.ShadowSQLiteDatabase;
+
 @SuppressWarnings("all")
 public class AndroidStorageTests {
+  public static void perform(final StoreTest test) {
+    final SQLiteConfiguration conf = AsyncMapAndorid.createDefaultConfiguration();
+    final SQLiteDatabase db = ShadowSQLiteDatabase.create(null);
+    AsyncMapAndorid.assertTable(db, conf);
+    Serializer<StreamSource, StreamDestination> _newJavaSerializer = SerializationJre.newJavaSerializer();
+    final Store<String, Object> map = AsyncMapAndorid.<Object>createMap(conf, _newJavaSerializer, db);
+    final Procedure1<ValueCallback<Success>> _function = new Procedure1<ValueCallback<Success>>() {
+      @Override
+      public void apply(final ValueCallback<Success> callback) {
+        SimpleCallback _asSimpleCallback = AsyncCommon.asSimpleCallback(callback);
+        map.start(_asSimpleCallback);
+      }
+    };
+    Async.<Success>waitFor(
+      ((Operation<Success>) new Operation<Success>() {
+          public void apply(ValueCallback<Success> callback) {
+            _function.apply(callback);
+          }
+      }));
+    test.test(map);
+    final Procedure1<ValueCallback<Success>> _function_1 = new Procedure1<ValueCallback<Success>>() {
+      @Override
+      public void apply(final ValueCallback<Success> callback) {
+        SimpleCallback _asSimpleCallback = AsyncCommon.asSimpleCallback(callback);
+        map.stop(_asSimpleCallback);
+        db.close();
+      }
+    };
+    Async.<Success>waitFor(
+      ((Operation<Success>) new Operation<Success>() {
+          public void apply(ValueCallback<Success> callback) {
+            _function_1.apply(callback);
+          }
+      }));
+  }
 }
--- src/main/xtend/de/mxro/async/map/android/tests/AndroidStorageTests.xtend
+++ src/main/xtend/de/mxro/async/map/android/tests/AndroidStorageTests.xtend
@@ -1,5 +1,34 @@
 package de.mxro.async.map.android.tests
 
+import android.database.sqlite.SQLiteDatabase
+import de.mxro.async.map.android.AsyncMapAndorid
+import de.mxro.async.map.android.SQLiteConfiguration
+import de.mxro.serialization.jre.SerializationJre
+import delight.async.AsyncCommon
+import delight.async.Operation
+import delight.async.callbacks.ValueCallback
+import delight.async.jre.Async
+import delight.functional.Success
+import delight.keyvalue.Store
+import delight.keyvalue.tests.StoreTest
+import org.robolectric.shadows.ShadowSQLiteDatabase
+
 class AndroidStorageTests {
+	
+	def static void perform(StoreTest test) {
+		val SQLiteConfiguration conf = AsyncMapAndorid::createDefaultConfiguration()
+		val SQLiteDatabase db = ShadowSQLiteDatabase::create(null)
+		AsyncMapAndorid::assertTable(db, conf)
+		val Store<String, Object> map = AsyncMapAndorid::createMap(conf, SerializationJre::newJavaSerializer(), db)
+		Async::waitFor((
+			[ValueCallback<Success> callback|map.start(AsyncCommon::asSimpleCallback(callback))] as Operation<Success>))
+			
+		test.test(map)	
+			
+			
+		Async::waitFor((
+			[ValueCallback<Success> callback|map.stop(AsyncCommon::asSimpleCallback(callback)) db.close()] as Operation<Success>)
+		)
+	}
 	
 }
