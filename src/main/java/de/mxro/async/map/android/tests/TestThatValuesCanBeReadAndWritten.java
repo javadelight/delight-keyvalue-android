0a2af2aa0ea527dc41fdd981e2d9e4cfa2bc2245
mxro <mxro@nowhere.com>
2016-06-14 11:05:31 +1000

    Improving Stellar DB Autocommit Tue Jun 14 11:05:31 AEST 2016

--- src/main/java/de/mxro/async/map/android/tests/TestThatValuesCanBeReadAndWritten.java
+++ src/main/java/de/mxro/async/map/android/tests/TestThatValuesCanBeReadAndWritten.java
@@ -40,7 +40,7 @@ public class TestThatValuesCanBeReadAndWritten {
     };
     Async.<Success>waitFor(
       ((Operation<Success>) new Operation<Success>() {
-          public void apply(ValueCallback<Success> callback) {
+          public void apply(ValueCallback<Success> null) {
             _function.apply(callback);
           }
       }));
